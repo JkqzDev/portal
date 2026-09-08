2d1f34a11b0a6121f3fa6878d83fc2a95a4bf8a7
JkqzDev <espulefjuan13@gmail.com>
2026-09-07 22:13:18 -0300

    ops

--- session/anticheat.go
+++ session/anticheat.go
@@ -1,8 +1,6 @@
 package session
 
 import (
-	"encoding/json"
-
 	"github.com/Weekom-UHC/anticheat-go/check"
 	"github.com/Weekom-UHC/anticheat-go/player"
 	"github.com/Weekom-UHC/anticheat-go/utils"
@@ -11,8 +9,6 @@ import (
 	"github.com/sandertv/gophertunnel/minecraft/text"
 )
 
-const anticheatNotifyIdentifier = "anticheat:notify"
-
 type anticheatHandler struct {
 	player.NopHandler
 	s *Session
@@ -20,50 +16,33 @@ type anticheatHandler struct {
 
 func (h anticheatHandler) HandleFlag(_ *dfevent.Context[struct{}], c check.Check, params map[string]any, _ *bool) {
 	name, variant := c.Name()
+	name += variant
 	player := h.s.conn.IdentityData().DisplayName
 
-	h.s.log.Infof(
-		"%s failed %s%s (%.1f/%.1f) %s",
-		player, name, variant, c.Violations(), c.MaxViolations(), utils.PrettyParameters(params, true),
-	)
+	h.s.log.Infof("%s failed %s (%.1f/%.1f) %s", player, name, c.Violations(), c.MaxViolations(), utils.PrettyParameters(params, true))
 
-	h.notify("flag", name+variant, player, text.Colourf(
-		"<grey>[</grey><red>AC</red><grey>]</grey> <yellow>%s</yellow> <grey>failed</grey> <white>%s%s</white> <grey>(%.1f/%.1f)</grey> <dark-grey>%s</dark-grey>",
-		player, name, variant, c.Violations(), c.MaxViolations(), utils.PrettyParameters(params, true),
+	h.broadcast(text.Colourf(
+		"<grey>[</grey><red>AntiCheat</red><grey>]</grey> <yellow>%s</yellow> <grey>is suspected of using</grey> <white>%s</white> <grey>(%.1f/%.1f)</grey>",
+		player, name, c.Violations(), c.MaxViolations(),
 	))
 }
 
 func (h anticheatHandler) HandlePunishment(_ *dfevent.Context[struct{}], c check.Check, message *string) {
 	name, variant := c.Name()
+	name += variant
 	player := h.s.conn.IdentityData().DisplayName
 
-	*message = text.Colourf("<red>Kicked by anticheat</red> <grey>(%s%s)</grey>", name, variant)
-	h.s.log.Infof("%s was kicked by the anticheat for %s%s", player, name, variant)
+	*message = text.Colourf("<red>Kicked by anticheat</red> <grey>(%s)</grey>", name)
+	h.s.log.Infof("%s was kicked by the anticheat for %s", player, name)
 
-	h.notify("punishment", name+variant, player, text.Colourf(
-		"<grey>[</grey><red>AC</red><grey>]</grey> <yellow>%s</yellow> <red>was kicked for</red> <white>%s%s</white>",
-		player, name, variant,
+	h.broadcast(text.Colourf(
+		"<grey>[</grey><red>AntiCheat</red><grey>]</grey> <yellow>%s</yellow> <red>was removed for using</red> <white>%s</white>",
+		player, name,
 	))
 }
 
-func (h anticheatHandler) notify(kind, check, player, message string) {
-	conn := h.s.ServerConn()
-	if conn == nil {
-		return
+func (h anticheatHandler) broadcast(message string) {
+	for _, s := range h.s.store.All() {
+		_ = s.conn.WritePacket(&packet.Text{TextType: packet.TextTypeRaw, Message: message})
 	}
-
-	payload, err := json.Marshal(map[string]string{
-		"type":    kind,
-		"check":   check,
-		"player":  player,
-		"message": message,
-	})
-	if err != nil {
-		return
-	}
-
-	_ = conn.WritePacket(&packet.ScriptMessage{
-		Identifier: anticheatNotifyIdentifier,
-		Data:       payload,
-	})
 }
