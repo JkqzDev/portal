fcf3257c8ffbda01ed65c1900605de4deaee8d38
JkqzDev <espulefjuan13@gmail.com>
2026-09-07 22:31:37 -0300

    feat: remove autopunishment

--- session/anticheat.go
+++ session/anticheat.go
@@ -19,24 +19,10 @@ func (h anticheatHandler) HandleFlag(_ *dfevent.Context[struct{}], c check.Check
 	name += variant
 	player := h.s.conn.IdentityData().DisplayName
 
-	h.s.log.Infof("%s failed %s (%.1f/%.1f) %s", player, name, c.Violations(), c.MaxViolations(), utils.PrettyParameters(params, true))
+	h.s.log.Infof("%s failed %s %s", player, name, utils.PrettyParameters(params, true))
 
 	h.broadcast(text.Colourf(
-		"<grey>[</grey><red>AntiCheat</red><grey>]</grey> <yellow>%s</yellow> <grey>is suspected of using</grey> <white>%s</white> <grey>(%.1f/%.1f)</grey>",
-		player, name, c.Violations(), c.MaxViolations(),
-	))
-}
-
-func (h anticheatHandler) HandlePunishment(_ *dfevent.Context[struct{}], c check.Check, message *string) {
-	name, variant := c.Name()
-	name += variant
-	player := h.s.conn.IdentityData().DisplayName
-
-	*message = text.Colourf("<red>Kicked by anticheat</red> <grey>(%s)</grey>", name)
-	h.s.log.Infof("%s was kicked by the anticheat for %s", player, name)
-
-	h.broadcast(text.Colourf(
-		"<grey>[</grey><red>AntiCheat</red><grey>]</grey> <yellow>%s</yellow> <red>was removed for using</red> <white>%s</white>",
+		"<grey>[</grey><red>AntiCheat</red><grey>]</grey> <yellow>%s</yellow> <grey>is suspected of using</grey> <white>%s</white>",
 		player, name,
 	))
 }
